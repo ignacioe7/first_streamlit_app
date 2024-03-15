2a45c0708908537ea60ec0f72e72d69ec1cfa2a3
ignacioe7 <92415047+ignacioe7@users.noreply.github.com>
2024-03-15 13:49:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,1 @@
-import streamlit as st
-
 import snowflake.connector 
-
-
-st.title('Página de inicio')
-
-
-st.subheader('Bienvenido a nuestra aplicación web')
-
-
-nombre = st.text_input('Introduce tu nombre')
-if st.button('Saludar'):
-    st.write(f'Hola, {nombre}!')
